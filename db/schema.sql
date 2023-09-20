b1a58cc6fc6d27370805001d65cbde31005abb07
spmoroney <spmoroney@att.net>
2023-09-19 21:29:34 -0400

    Begin defining server and schema, update gitignore

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department {
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+};
+
---
+++ server.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+require('dotenv').config();
+
