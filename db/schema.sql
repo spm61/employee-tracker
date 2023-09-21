e631cd2e50686ff449c6dabcb5b69c148e727772
spmoroney <spmoroney@att.net>
2023-09-21 18:42:37 -0400

    finish defining db schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,20 @@ CREATE TABLE department {
     name VARCHAR(30) NOT NULL
 };
 
+CREATE TABLE role {
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DEC NOT NULL,
+    department_id INTEGER,
+    INDEX dep_ind (department_id),
+    CONSTRAINT foreignkey_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+};
+
+CREATE TABLE employee {
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL.
+    role_id INT,
+    INDEX role_index (role_id),
+    CONSTRAINT foreignkey_role FOREIGN KEY (role_id) referenes role(id) ON DELETE SET NULL
+};
